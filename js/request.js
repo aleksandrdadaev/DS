cdecf5f38a731775b1db25cda4373008de6e3f1f
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2022-10-24 13:49:47 +0700

    Добавил функционал открытия и скрытия формы заявки

---
+++ js/request.js
@@ -0,0 +1,30 @@
+const requestBg = document.querySelector('#request');
+const request = requestBg.firstElementChild;
+const openRequestButtons = document.querySelectorAll('#open-request');
+const closeRequestButton = request.querySelector('#request-close');
+
+openRequestButtons.forEach(button => {
+	button.addEventListener('click', e => {
+		e.preventDefault();
+		requestBg.classList.add('request-bg_active');
+		request.classList.add('request_active');
+		document.body.style.overflowY = 'hidden';
+	});
+});
+
+closeRequestButton.addEventListener('click', e => {
+	e.preventDefault();
+	closeRequest();
+});
+
+document.addEventListener('click', e => {
+	if (e.target === requestBg) {
+		closeRequest();
+	}
+});
+
+function closeRequest() {
+	requestBg.classList.remove('request-bg_active');
+	request.classList.remove('request_active');
+	document.body.style.overflowY = '';
+}
