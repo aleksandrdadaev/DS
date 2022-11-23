65318e0a6821dc1e9fd860e940d73b8401040713
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2022-11-23 12:19:27 +0700

    Заменил картинки в первом блоке возможностей и анимация второго блока возможностей

--- js/opportunities.js
+++ js/opportunities.js
@@ -1,5 +1,9 @@
 const design = document.querySelector('#opportunities-design');
 const designHeight = design.clientHeight;
+const gear = document.querySelector('#develop-gear');
+const leftArrow = document.querySelector('#develop-left-arrow');
+const rightArrow = document.querySelector('#develop-right-arrow');
+const slash = document.querySelector('#develop-slash');
 
 setTimeout(() => {
 	design.style.transform = `translateY(-${
@@ -10,3 +14,19 @@ setTimeout(() => {
 		design.style.transform = 'translateY(0)';
 	}, 2500);
 }, 2000);
+
+setTimeout(() => {
+	gear.style.transform = `rotate(360deg)`;
+
+	setTimeout(() => {
+		leftArrow.style.transform = `translateX(-5px)`;
+		rightArrow.style.transform = `translateX(5px)`;
+		slash.style.transform = `scale(1.1)`;
+	}, 2000);
+
+	setTimeout(() => {
+		leftArrow.style.transform = `translateX(0)`;
+		rightArrow.style.transform = `translateX(0)`;
+		slash.style.transform = `scale(1)`;
+	}, 2300);
+}, 7000);
